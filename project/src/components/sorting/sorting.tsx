3145351827b1a7c42aa859a15511d4ffd741613f
Konstantin <kosvadmail@atomlc.com.ua>
2022-11-29 02:56:45 +0100

    Добавляет фильтрацию

--- project/src/components/sorting/sorting.tsx
+++ project/src/components/sorting/sorting.tsx
@@ -1,24 +1,55 @@
+import { SortingTypes } from '../../const';
+import { useAppDispatch } from '../../hooks';
+import { changeSortType, getOffersAction } from '../../store/actions/action';
+
 function Sorting(): JSX.Element {
+  const dispatch = useAppDispatch();
   return (
     <form className="places__sorting" action="#" method="get">
       <span className="places__sorting-caption">Sort by</span>
-      <span className="places__sorting-type" tabIndex={0}>
+      <span className="places__sorting-type" >
         Popular
-        <svg className="places__sorting-arrow" width="7" height="4">
+        <svg onClick={() => {
+          const placesOptions = document.querySelector('.places__options');
+          if (placesOptions) {
+            placesOptions.classList.toggle('places__options--opened');
+          }
+        }} className="places__sorting-arrow" width="7" height="4"
+        >
           <use xlinkHref="#icon-arrow-select"></use>
         </svg>
       </span>
-      <ul className="places__options places__options--custom places__options--opened">
-        <li className="places__option places__option--active" tabIndex={0}>
+      <ul className="places__options places__options--custom">
+        <li className="places__option places__option--active" onClick={() => {
+          dispatch(changeSortType(SortingTypes.Popular));
+
+          dispatch(getOffersAction());
+        }}
+        >
           Popular
         </li>
-        <li className="places__option" tabIndex={0}>
+
+        <li className="places__option" onClick={() => {
+          dispatch(changeSortType(SortingTypes.LowHigh));
+          dispatch(getOffersAction());
+        }}
+        >
           Price: low to high
         </li>
-        <li className="places__option" tabIndex={0}>
+
+        <li className="places__option" onClick={() => {
+          dispatch(changeSortType(SortingTypes.HighLow));
+          dispatch(getOffersAction());
+        }}
+        >
           Price: high to low
         </li>
-        <li className="places__option" tabIndex={0}>
+
+        <li className="places__option" onClick={() => {
+          dispatch(changeSortType(SortingTypes.TopRated));
+          dispatch(getOffersAction());
+        }}
+        >
           Top rated first
         </li>
       </ul>
--- project/src/const.tsx
+++ project/src/const.tsx
@@ -15,6 +15,13 @@ enum CitiesNames {
   Dusseldorf = 'Dusseldorf',
 }
 
+enum SortingTypes {
+  Popular,
+  LowHigh,
+  HighLow,
+  TopRated,
+}
+
 const AMSTERDAM_CITY: City = {
   latitude: 52.3740300,
   longitude: 4.8896900,
@@ -22,12 +29,12 @@ const AMSTERDAM_CITY: City = {
   name: CitiesNames.Amsterdam,
 };
 
-const CITIES_LIST: string[] = [CitiesNames.Paris, CitiesNames.Cologne, CitiesNames.Brussels, CitiesNames.Amsterdam, CitiesNames.Hamburg, CitiesNames.Dusseldorf,];
+const CITIES_LIST: CitiesNames[] = [CitiesNames.Paris, CitiesNames.Cologne, CitiesNames.Brussels, CitiesNames.Amsterdam, CitiesNames.Hamburg, CitiesNames.Dusseldorf,];
 
 const URL_MARKER_DEFAULT =
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/pin.svg';
+  'img/pin.svg';
 
 const URL_MARKER_CURRENT =
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/main-pin.svg';
+  'img/pin-active.svg';
 
-export { Paths, AMSTERDAM_CITY, URL_MARKER_DEFAULT, URL_MARKER_CURRENT, CITIES_LIST, CitiesNames};
+export { Paths, AMSTERDAM_CITY, URL_MARKER_DEFAULT, URL_MARKER_CURRENT, CITIES_LIST, CitiesNames, SortingTypes};
